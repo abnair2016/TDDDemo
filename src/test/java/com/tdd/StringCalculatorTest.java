3c237b91b89691186291fbf2137404ce77fb8170
Abhilash Nair <abnair2016@gmail.com>
2017-01-17 22:08:13 +0000

    Amended tests

--- src/test/java/com/tdd/StringCalculatorTest.java
+++ src/test/java/com/tdd/StringCalculatorTest.java
@@ -8,27 +8,31 @@ import org.junit.Test;
 public class StringCalculatorTest {
 
     /**
+      * https://github.com/abnair2016/TDDDemo.git
+     *
      * Create a simple String calculator with a method int Add(string numbers).
      *
-     * 1: An empty string returns zero
-     * 2: Null returns zero
-     * 3: A single number returns the value
-     * 4: Two numbers, comma delimited, returns the sum
-     * 5: Two numbers, newline delimited, returns the sum
-     * 6: Three numbers, delimited either way, returns the sum
-     * 7: Negative numbers throw an exception
-     * 8: Numbers greater than 1000 are ignored
+     * 1: A single number returns the value
+     * 2: Another single number returns respective value
+     * 3: An empty string returns zero
+     * 4: An empty string with whitespaces returns zero
+     * 5: Null returns zero
+     * 6: Two numbers, comma delimited, returns the sum
+     * 7: Two numbers, newline delimited, returns the sum
+     * 8: Three numbers, delimited either way, returns the sum
+     * 9: Single Negative number, throw an exception and list the negative number
+     * 10: Multiple Negative numbers, throw an exception and list all negative numbers
+     * 11: Numbers greater than 1000 are ignored
      *
      */
 
     @Before
     public void setUp(){
-
     }
 
     @Test
     public void testName() throws Exception{
-        assertEquals("","");
+        assertEquals("", "");
     }
 
 }
